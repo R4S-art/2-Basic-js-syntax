a3befd3e1d7ed4cb827b05049fa14a16a1fb0fe1
Dezir <j.djok9@gmail.com>
2021-09-12 21:04:25 +0300

    Функции

--- file.js
+++ file.js
@@ -34,8 +34,31 @@ function rangeOdd(start, end) {
     }
     console.log(arr);
 
-
+return arr;
 }
 rangeOdd(15,30);
 
 //Функции
+
+const average =(a,b) =>{
+    return (a+b)/2;
+}
+console.log(average(10,20));
+
+const squere =(a)=>{
+    return a*a;
+}
+
+const cube = (a) => {
+    return a*a*a;
+}
+
+function calculete (end){
+    let arr=[];
+    for(let i =0;i<end;i++){
+        arr.push(average(squere(i),cube(i)));
+
+    }
+console.log(arr);
+}
+calculete(10);
